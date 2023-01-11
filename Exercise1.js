a58362e2b948ff691a4b0b97c52d41c65a9be0a5
bropel <bryceopel@gmail.com>
2023-01-10 20:12:29 -0500

    Upload exercise 1 solution

---
+++ Exercise1.js
@@ -0,0 +1,9 @@
+/* 
+Declare variables for your name, age, and favorite color, and assign appropriate values to them. Then, use string concatenation or template literals to print a sentence introducing yourself, such as "Hi, my name is John and I am 30 years old. My favorite color is blue."
+*/
+
+const name = 'Bryce';
+const age = 28;
+const favColor = 'red';
+
+console.log(`Hi, my name is ${name} and I am ${age} years old. My favorite color is ${favColor}.`);
