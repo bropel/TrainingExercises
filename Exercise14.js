34b92ceab739ce449d0e88b6711db64f6f93be71
bropel <bryceopel@gmail.com>
2023-01-11 11:29:42 -0500

    Adding exercise 14 solution *not my code*

---
+++ Exercise14.js
@@ -0,0 +1,27 @@
+/*
+Multiplication table: Write a program that generates a multiplication table for the numbers 1 to 10. The table should have 10 rows, one for each number from 1 to 10, and 10 columns, one for each number from 1 to 10. Use a loop to solve this exercise.
+*/
+
+//NOT MY CODE
+
+/*
+var result = 'x ';
+for (var i = 0; i < 11; i++) {
+
+    for (var j = 0; j < 11; j++) {
+
+        if(i == 0 && j > 0){
+          result += '[' + j + ']';
+        } 
+        else if(j == 0 && i>0){
+          result += '[' + i + '] ';
+        } 
+        else if(i>0 && j>0){
+        result += (i*j) + ' ';
+        }
+    }
+    result += '\n'
+}
+
+console.log(result);
+*/
